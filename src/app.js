b56a2f82d894347ca4c51b6d853dcc6709dcf3a8
Jhony <132751810+Jhony0000@users.noreply.github.com>
2025-01-02 15:41:22 +0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,15 +17,15 @@ app.use(cookieParser());
 // Routes import
 import userRouter from './routes/user.Router.js';
 import videoRouter from './routes/video.Router.js';
-import messageRouter from './routes/message.router.js';
-import blogRouter from './routes/blog.routs.js';
-import notificationRouter from './routes/notifications.routes.js';
+// import messageRouter from './routes/message.router.js';
+// import blogRouter from './routes/blog.routs.js';
+// import notificationRouter from './routes/notifications.routes.js';
 
 // Regular user routes
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/videos', videoRouter);
-app.use('/api/v1/messages', messageRouter);
-app.use('/api/v1/blogs', blogRouter);
-app.use('/api/v1/notifications', notificationRouter);
+// app.use('/api/v1/messages', messageRouter);
+// app.use('/api/v1/blogs', blogRouter);
+// app.use('/api/v1/notifications', notificationRouter);
 
 export default app;
