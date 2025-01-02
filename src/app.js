82977b3d118a7106c504b23ee176f461d38dfdf8
Jhony <132751810+Jhony0000@users.noreply.github.com>
2025-01-02 14:53:30 +0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,37 +1,31 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+// Middleware setup
 app.use(cors({
-    origin:process.env.CORSE_ORIGIN,
-    credentials:true,
-}))
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+app.use(express.json({ limit: '10gb' }));
+app.use(express.urlencoded({ extended: true, limit: '10gb', parameterLimit: 15000 }));
+app.use(express.static('public'));
+app.use(cookieParser());
 
-
-
-app.use(express.json({limit: '10gb'}))
-app.use(express.urlencoded({extended: true , limit: '10gb',parameterLimit:15000}))
-app.use(express.static('public'))
-app.use(cookieParser())
-
-// routs import 
-import userRouter from './routes/user.Router.js'
-import videoRouter from './routes/video.Router.js'
-import messageRouter from './routes/message.router.js'
-import blogRouter from './routes/blog.routs.js'
+// Routes import
+import userRouter from './routes/user.Router.js';
+import videoRouter from './routes/video.Router.js';
+import messageRouter from './routes/message.router.js';
+import blogRouter from './routes/blog.routs.js';
 import notificationRouter from './routes/notifications.routes.js';
 
-//  reguller user 
-app.use('/api/v1/users' ,userRouter)
-app.use('/api/v1/videos',videoRouter)
-app.use('/api/v1/message',messageRouter)
-app.use('/api/v1/blog',blogRouter)
-app.use('/api/v1/notification',notificationRouter)
+// Regular user routes
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/videos', videoRouter);
+app.use('/api/v1/messages', messageRouter);
+app.use('/api/v1/blogs', blogRouter);
+app.use('/api/v1/notifications', notificationRouter);
 
-// bussiness user 
-
-
-export default app
+export default app;
